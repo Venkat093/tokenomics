ccba5fbbf00d2cd7a4e266f671842158560e8722
Venkat Sai <saivenkat093@gmail.com>
2022-06-24 13:48:00 +0530

    tokenomics final commit

--- src/components/Stake.jsx
+++ src/components/Stake.jsx
@@ -34,7 +34,7 @@ const Stake = ({ auc, acc, web3main }) => {
 				'0xadc22D2bF20d69243c039306bF2c301Ea2c49F14'
 			);
 
-			const stakingCOntract = '0xb57ca6E8EcBbABFD613c74f663C3c8973E40c310';
+			const stakingCOntract = '0xcfD8CAb7e15688003e43C24FF56d854A38EFa83b';
 
 			let approveAMount = web3main.utils.toBN(
 				fromExponential(parseFloat(_amount) * Math.pow(10, 25))
@@ -62,7 +62,7 @@ const Stake = ({ auc, acc, web3main }) => {
 				'0xadc22D2bF20d69243c039306bF2c301Ea2c49F14'
 			);
 
-			const stakingCOntract = '0xb57ca6E8EcBbABFD613c74f663C3c8973E40c310';
+			const stakingCOntract = '0xcfD8CAb7e15688003e43C24FF56d854A38EFa83b';
 
 			let amount = web3main.utils.toBN(
 				fromExponential(parseFloat(_amount) * Math.pow(10, 18))
@@ -92,7 +92,7 @@ const Stake = ({ auc, acc, web3main }) => {
 			console.log('ccc', userwalletaddresss);
 			let staking = new web3main.eth.Contract(
 				Stakee,
-				'0xb57ca6E8EcBbABFD613c74f663C3c8973E40c310'
+				'0xcfD8CAb7e15688003e43C24FF56d854A38EFa83b'
 			);
 
 			let amount = web3main.utils.toBN(
@@ -117,7 +117,7 @@ const Stake = ({ auc, acc, web3main }) => {
 
 			let staking = new web3main.eth.Contract(
 				Stakee,
-				'0xb57ca6E8EcBbABFD613c74f663C3c8973E40c310'
+				'0xcfD8CAb7e15688003e43C24FF56d854A38EFa83b'
 			);
 
 			let amount = web3main.utils.toBN(
@@ -151,7 +151,7 @@ const Stake = ({ auc, acc, web3main }) => {
 
 			let staking = new web3main.eth.Contract(
 				Stakee,
-				'0xb57ca6E8EcBbABFD613c74f663C3c8973E40c310'
+				'0xcfD8CAb7e15688003e43C24FF56d854A38EFa83b'
 			);
 
 			staking.methods
@@ -183,7 +183,7 @@ const Stake = ({ auc, acc, web3main }) => {
 			let userwalletaddresss = accounts[0];
 			let staking = new web3main.eth.Contract(
 				Stakee,
-				'0xb57ca6E8EcBbABFD613c74f663C3c8973E40c310'
+				'0xcfD8CAb7e15688003e43C24FF56d854A38EFa83b'
 			);
 			staking.methods
 				.stakeBalanceOfUser(userwalletaddresss)
@@ -202,7 +202,7 @@ const Stake = ({ auc, acc, web3main }) => {
 
 			let staking = new web3main.eth.Contract(
 				Stakee,
-				'0xb57ca6E8EcBbABFD613c74f663C3c8973E40c310'
+				'0xcfD8CAb7e15688003e43C24FF56d854A38EFa83b'
 			);
 
 			staking.methods
